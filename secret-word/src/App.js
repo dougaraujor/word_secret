044adad34a165b7ece042b69b000dea048a64682
Douglas Araujo <dougaraujor@gmail.com>
2024-07-04 23:38:19 -0300

    Criando components e css de outras paginas da operação

--- secret-word/src/App.js
+++ secret-word/src/App.js
@@ -1,11 +1,36 @@
-
+// Css
 import './App.css';
+
+// React
+import {useCallBack, useEffect, useState} from "react";
+
+// Componentes
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+// Data
+import { wordsList } from './data/words';
+
+
+
+const stages =[
+  {id:1,name:"start"},
+  {id:2,name:"game"},
+  {id:3,name:"end"},
+]
 
 function App() {
+
+  const [gameStage, setGameState] = useState(stages[0].name);
+  const [words] = useState(wordsList)
+
+  console.log(words)
   return (
     <div className="App">
-     <StartScreen/>
+      {gameStage === 'start' && <StartScreen/>}
+      {gameStage === 'game' && <Game/>}
+      {gameStage === 'end' && <GameOver/>}
     </div>
   );
 }
