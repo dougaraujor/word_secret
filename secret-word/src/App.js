63910dcfd63dbaf31113d35a148259d8d7cb36af
Douglas Araujo <dougaraujor@gmail.com>
2024-07-15 20:54:47 -0300

    Lógica Game over

--- secret-word/src/App.js
+++ secret-word/src/App.js
@@ -18,6 +18,8 @@ const stages = [
   { id: 3, name: "end" },
 ];
 
+const guessesQty = 3;
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
@@ -26,7 +28,7 @@ function App() {
   const [letter, setLetters] = useState([]);
   const [guessedLetters, setGuessedLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
-  const [guesses, setGuessed] = useState(3);
+  const [guesses, setGuessed] = useState(guessesQty);
   const [score, setScore] = useState(0);
 
   const pickWordAndCategory = () => {
@@ -83,13 +85,26 @@ function App() {
         ...actualWrongLetters,
         normalizedLetter,
       ]);
+      setGuessed((actualGuesses) => actualGuesses - 1);
     }
-    console.log(guessedLetters);
-    console.log(wrongLetters);
   };
+
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  };
+  useEffect(() => {
+    if (guesses <= 0) {
+      // reset all states
+      clearLetterStates();
+      setGameStage(stages[2].name);
+    }
+  }, [guesses]);
 
   // Restart jogo
   const retry = () => {
+    setScore(0);
+    setGuessed(guessesQty);
     setGameStage(stages[0].name);
   };
   return (
